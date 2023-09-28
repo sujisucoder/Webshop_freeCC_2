786133531cb7c20864b338750954ab4c457b97aa
sujith surendran <sujithsurendran4433@gmail.com>
2023-09-28 11:28:46 +0530

    added material, tailwind, and mat-toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [HeaderComponent]
 })
 export class AppComponent {
   title = 'Webshop_freeCC_2';
