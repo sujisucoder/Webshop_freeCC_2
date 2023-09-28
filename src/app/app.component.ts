864878ace5e9afba5777767fc8ee37840c8b716e
sujith surendran <sujithsurendran4433@gmail.com>
2023-09-28 14:32:25 +0530

    added cart , currency , routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, HomeComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'Webshop_freeCC_2';
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+
+
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatMenuModule, MatIconModule, MatBadgeModule, RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
